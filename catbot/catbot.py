ed8887ae816ebd0533ea30db53d74087d772b0fb
asher <asaunde78@gmail.com>
2023-03-06 03:12:58 -0500

    added the ip sending functionality

--- catbot/catbot.py
+++ catbot/catbot.py
@@ -2,6 +2,11 @@ import discord, asyncio, random
 from discord.ext import commands 
 import datetime
 import tenorpy
+import subprocess 
+ip = subprocess.check_output("hostname -I".split()).decode().strip("\n")
+
+print(ip)
+
 description = 'This is CatBot'
 bot = commands.Bot(command_prefix='~', description=description)
 letters = {'a': '🇦', 'b': '🇧', 'c': '🇨', 'd': '🇩', 'e': '🇪', 'f': '🇫', 'g': '🇬', 'h': '🇭', 'i': '🇮', 'j': '🇯', 'k': '🇰', 'l': '🇱', 'm': '🇲', 'n': '🇳', 'o': '🇴', 'p': '🇵', 'q': '🇶', 'r': '🇷', 's': '🇸', 't': '🇹', 'u': '🇺', 'v': '🇻', 'w': '🇼', 'x': '🇽', 'y': '🇾', 'z': '🇿'}
@@ -32,7 +37,7 @@ async def on_ready():
         channel = bot.get_channel(796072509039837207)
         greeting = random.randint(1, 7)
         greetings = {1:"MIAOU!", 2:"CatBot reporting for duty!", 3:"CatBot back online!",4:"CatSystems turning on...", 5:"I am a CatBot", 6:"Miaou!", 7:"CatOS loading..."}
-        await channel.send(greetings[greeting])
+        await channel.send(greetings[greeting]+ "\n IP: pi@" +ip)
 @bot.event
 async def on_message(message):
     global word
@@ -59,11 +64,13 @@ async def on_message(message):
         await message.channel.send(t.random(gif))
     for item in msg:
         if isWord(item, word) or item == bot.user.mention:
+            for a in list(word):
+                if(a in letters):
+                    await message.add_reaction(letters[a])
+        if item == bot.user.mention:
             responses = {1:"! I'm CatBot!", 2:"! You rang?", 3:"! That's my name. Don't wear it out!", 4:"! Did you just say my name?", 5:""}
             intros = {1:"Hi, ", 2:"Yo yo, ", 3:"Wazaaa, ", 4:"What's up, ", 5:"Yo, "}
-            for a in list(word):
-                await message.add_reaction(letters[a])
-            #await message.channel.send(intros[random.randint(1,5)]+ message.author.mention + responses[random.randint(1,5)])
+            await message.channel.send(intros[random.randint(1,5)]+ message.author.mention + responses[random.randint(1,5)])
     await bot.process_commands(message)
 from token_folder import token
 bot.run(token)
