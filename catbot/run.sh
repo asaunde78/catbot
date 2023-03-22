9a45b38550da6434a3bcd70d7cf65d48b6821d0d
asaunde78 <saundersasher78@gmail.com>
2023-03-21 23:23:55 -0400

    reorganized some files to get ready for cogs

--- catbot/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-file=$1;
-source=$2
-echo "running file: $file.py";
-
-#screen -d -m -S $file python3 $file.py -s $source;
-screen -X -S $file quit;
-screen -d -m -S $file python3 $file.py -s $source;
-
--- catbot/run_catbot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/asher/catbot/catbot/
-
-screen -m -d -S catbot python3 manage.py
