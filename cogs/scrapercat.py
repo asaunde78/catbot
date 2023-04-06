e8959deab00709e0862fee1e3295ff794376ccb1
asaunde78 <saundersasher78@gmail.com>
2023-04-06 19:45:42 -0400

    scraper cat now has functionality to upload the pics as a gif

--- cogs/scrapercat.py
+++ cogs/scrapercat.py
@@ -5,14 +5,51 @@ import os
 name = os.path.splitext(os.path.basename(__file__))[0]
 import sys
 sys.path.insert(1, '/home/asher/catscraper')
+sys.path.insert(1, '/home/asher/clippy')
+import requests
+
+from clip import Clippy
+
+import shutil
 from runner import scraper
 class Scrapercat(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
-        self.scraper = scraper(workers=5)
-    @app_commands.command(name="getimages", description="Experimental :3")
+        self.folder = "scrapedpics"
+        self.c = Clippy(folder=self.folder)
+        self.scraper = scraper(workers=4)
+    @app_commands.command(name="getimages",description="Generates a gif of a google images search using my own google search scraper")
     async def getimages(
+        self,
+        interaction: discord.Interaction,
+        search: str,
+        show: bool
+    ):
+        await interaction.response.defer(ephemeral=not show)
+        search  += "filetype:jpg"
+    
+        images = self.scraper.genimages(search,3)
+        
+        for filename in os.listdir(self.folder):
+                    file_path = os.path.join(self.folder, filename)
+                    try:
+                        if os.path.isfile(file_path) or os.path.islink(file_path):
+                            os.unlink(file_path)
+                        elif os.path.isdir(file_path):
+                            shutil.rmtree(file_path)
+                    except Exception as e:
+                        print('Failed to delete %s. Reason: %s' % (file_path, e))
+        for index,link in enumerate(images):
+            print(link)
+            with requests.get(link, allow_redirects=True) as response, open(self.folder + f"/image({index}).jpg", 'wb') as f:
+                #print(response.text)
+                data = response.content
+                f.write(data)
+        print("done writing")
+        gif = self.c.imagestogif("image(%d).jpg",framerate=24)
+        await interaction.followup.send(file=discord.File(self.folder + "/" + gif))
+    @app_commands.command(name="getlinks", description="Experimental :3")
+    async def getlinks(
         self,
         interaction: discord.Interaction,
         search: str
