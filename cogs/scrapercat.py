6cd7e3168c16099cbbe69e5b5388d056a8290760
asaunde78 <saundersasher78@gmail.com>
2023-04-12 01:11:13 -0400

    updated the scrapercat to include the new version of CatScraper

--- cogs/scrapercat.py
+++ cogs/scrapercat.py
@@ -5,6 +5,7 @@ from typing import Literal, Optional
 import os
 name = os.path.splitext(os.path.basename(__file__))[0]
 import sys
+sys.path.insert(1, '/home/asher/catscraper/blockerextension.crx')
 sys.path.insert(1, '/home/asher/catscraper')
 sys.path.insert(1, '/home/asher/clippy')
 import requests
@@ -18,7 +19,7 @@ class Scrapercat(commands.Cog):
         self.bot = bot
         self.folder = "scrapedpics"
         self.c = Clippy(folder=self.folder)
-        self.scraper = scraper(workers=3)
+        self.scraper = scraper(workers=2,server=True,folder=self.folder)
     @app_commands.command(name="getimages",description="Generates a gif of a google images search using my own google search scraper")
     async def getimages(
         self,
@@ -31,27 +32,10 @@ class Scrapercat(commands.Cog):
         begin = time.time()
         await interaction.response.defer(ephemeral=not show)
         
-        search  += f" filetype:{ftype}"
     
-        images = self.scraper.genimages(search,3)
-        
-        for filename in os.listdir(self.folder):
-            file_path = os.path.join(self.folder, filename)
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except Exception as e:
-                print('Failed to delete %s. Reason: %s' % (file_path, e))
-        for index,link in enumerate(images):
-            print(link)
-            with requests.get(link, allow_redirects=True) as response, open(self.folder + f"/image({index}).{ftype}", 'wb') as f:
-                #print(response.text)
-                data = response.content
-                f.write(data)
-        print("done writing")
-        gif = self.c.imagestogif(f"image(%d).{ftype}",frametiming=frametiming)
+        self.scraper.genimages(search,6)
+    
+        gif = self.c.imagestogif(f"image(%d).{ftype}",filetype=ftype,glob=True,frametiming=frametiming)
         end = time.time()
         print(f"[INFO] Generating the pictures took {end-begin} seconds")
         await interaction.followup.send(file=discord.File(self.folder + "/" + gif))
