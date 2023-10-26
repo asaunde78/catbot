acb6eb042d51de1dc16862fc40e83a83c508d275
asaunde78 <saundersasher78@gmail.com>
2023-10-25 21:43:39 -0400

    add videokat

---
+++ cogs/videokat.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import random
+
+import os,shutil
+name = os.path.splitext(os.path.basename(__file__))[0]
+import sys
+
+    # caution: path[0] is reserved for script path (or '' in REPL)
+
+
+sys.path.insert(1, '/home/asher/glimpse')
+from glimpse import glimpser
+class Videokat(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.guild = self.bot.myguild
+        self.glimpser = glimpser()
+
+    @app_commands.command(name="glimpse",description="Take a glimpse of a youtube video")
+    async def glimpse(
+        self,
+        interaction: discord.Interaction,
+        url: str,
+        clips : int, 
+        time : float = 0.0,
+        sort :bool = False, 
+        random_gap :float =10.0
+    ) -> None:
+        
+        await interaction.response.defer(ephemeral=False)
+        self.glimpser.genVideo(
+            url,
+            clips, 
+            time,
+            sort, 
+            random_gap
+        )
+        
+        
+        await interaction.followup.send(file=discord.File("output_demuxer.mp4"))
+        
+
+        
+    
+async def setup(bot: commands.Bot):
+    global name
+    await bot.add_cog(Videokat(bot))
+    #print(f"Guild: {bot.myguild}")
+
+    print(f"{name} loaded")
+
+    channel = bot.get_channel(796072509039837207)
+    message = await channel.fetch_message(bot.message)
+    await message.add_reaction("🎥")
+    e = discord.Embed(title=message.embeds[0].title)
+    
+    e.set_footer(text=message.embeds[0].footer.text.replace(f"Waiting for: {name}",f"{name} on!"))
+    await message.edit(embed=e)
+    
